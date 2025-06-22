e07e2f6c44abc94523aef664ef9ba19e86a7a352
Asheem Rahman <mhdasheem@gmail.com>
2025-06-23 02:01:31 +0530

    Fea : Expert session schedule

---
+++ src/model/expert/sessionSchema.ts
@@ -0,0 +1,54 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface ISession extends Document {
+    userId: mongoose.Types.ObjectId;
+    expertId: mongoose.Types.ObjectId;
+    availabilityId: mongoose.Types.ObjectId;
+    meetingLink?: string;
+    status: 'upcoming' | 'completed' | 'missed';
+    bookedAt: Date;
+    startedAt?: Date;
+    endedAt?: Date;
+}
+
+const SessionSchema: Schema<ISession> = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    expertId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Expert',
+        required: true
+    },
+    availabilityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ExpertAvailability',
+        required: true
+    },
+    meetingLink: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ['upcoming', 'completed', 'missed'],
+        default: 'upcoming'
+    },
+    bookedAt: {
+        type: Date,
+        default: Date.now
+    },
+    startedAt: {
+        type: Date
+    },
+    endedAt: {
+        type: Date
+    },
+});
+
+
+const Session = mongoose.model<ISession>('Session', SessionSchema);
+
+
+export { Session, ISession };
